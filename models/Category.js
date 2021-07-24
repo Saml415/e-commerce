6027ac31dc280371e6e40b6d8d0c52fb06c58f3d
Sam Lloyd <bobdoe784@gmail.com>
2021-07-24 13:11:18 -0700

    Associations made for models

--- models/Category.js
+++ models/Category.js
@@ -4,19 +4,6 @@ const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
-// {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   location_name: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   }
-// },
-
 Category.init(
   {
     // define columns
