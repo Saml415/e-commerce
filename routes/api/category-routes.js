f09c1d3cbff05b118c14ba82442b63d21257f0fd
Sam Lloyd <bobdoe784@gmail.com>
2021-07-24 20:39:23 -0700

    Tag route created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,9 +37,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const categoryData = await Category.create({
-      product_id: req.body.product_id,
-    });
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
