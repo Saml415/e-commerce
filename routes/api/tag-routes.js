2ef5b8ae2a1106baeb09a5cf33640fc227ebd8c8
Sam Lloyd <bobdoe784@gmail.com>
2021-07-26 16:40:37 -0700

    Fixed bug wit delete tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,21 +63,21 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  try{
-    const tagData = await Tag.destroy(req.body, {
+  try {
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       },
     });
-    if (!tagData){
-      res.status(404).json({message: "No tag with this id!"});
+    if (!tagData) {
+      res.status(404).json({ message: "No tag with this id!" });
       return;
     }
     res.status(200).json(tagData);
-  }catch (err){
+  } catch (err) {
     res.status(500).json(err);
   }
-  
+
   // delete on tag by its `id` value
 });
 
